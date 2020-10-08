581521b7ac1e30e6958376cfc3f6debcc3ad9b36
Thao Vu <hoaithao1710@gmail.com>
2020-10-07 19:14:38 -0700

    Initial Commit: Blank methods for AddingMachine class

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,48 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    public AddingMachine(){
+        /*
+         *
+         */
+        total = 0;
+
+    }
+
+    public int getTotal(){
+        /* This method return
+         * the total result
+         */
+        return 0;
+    }
+
+    public void add(int value){
+        /* This method takes in an integer
+         * and add to the global variable total
+         */
+
+    }
+
+    public void subtract(int value){
+        /* This method takes in an integer
+         * and subtract to the global variable total
+         */
+
+    }
+
+    public String toString(){
+        /* This method print out
+         * the final result statement
+         */
+        return "";
+    }
+
+    public void clear(){
+        /* This method clears out the program and reset the variable total
+         *
+         */
+
+    }
+}
